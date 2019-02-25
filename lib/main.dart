97b634585eaac9eb1ed7f64bdbadc5f53e72e48f
dbenitez-bcn <dbr.bcn@gmail.com>
2019-02-25 21:58:05 +0100

    finally done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:astra/star.dart';
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,35 +24,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<StarModel> _starsModels;
+  List<Star> _starsModels;
+  Star _starSelected;
+  PageController _pageController;
+  Image imageNew;
+  TextEditingController tfTitle = TextEditingController();
+  TextEditingController tfDesc = TextEditingController();
+
+
+  void goToPage(int page) {
+    _pageController.animateToPage(page,
+        duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
+  }
 
   @override
   void initState() {
-    _starsModels = [
-      StarModel(
-        title: "Starting Astra",
-        description:
-        "Start coding the app from scratch, just makeing the idea real.",
-        starIcon: "assets/greenStar.flr",
-        date: DateTime.parse("2019-02-24"),
-        image: Image.asset("assets/starImage1.jgep"),
-      ),
-      StarModel(
-        title: "First bug found D:",
-        description:
-        "I used a bad callback on .map function. (FIXED)",
-        starIcon: "assets/greenStar.flr",
-        date: DateTime.parse("2019-02-24"),
-        image: Image.asset("assets/starImage2.png"),
-      ),
-    ];
+    _starsModels = [];
+    _pageController = PageController(
+      initialPage: 1,
+    );
+    imageNew = Image.asset("assets/noImage.png");
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildBody(),
+      floatingActionButton: _buildFab(),
     );
   }
 
@@ -60,31 +65,139 @@ class _MyHomePageState extends State<MyHomePage> {
     return SizedBox.expand(
       child: Stack(
         children: <Widget>[
-          _buildBg(),
-          _buildStars(),
+          Positioned.fill(
+            child: _buildFlareActor("assets/astra.flr", "main"),
+          ),
+          _buildPageView(),
         ],
       ),
     );
   }
 
-  Widget _buildBg() {
-    return Positioned.fill(
-      child: FlareActor(
-        "assets/astra.flr",
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: "main",
-      ),
+  Widget _buildFab() {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        goToPage(2);
+      },
+    );
+  }
+
+  Widget _buildPageView() {
+    return PageView(
+      controller: _pageController,
+      children: <Widget>[
+        _buildSelected(),
+        _buildStars(),
+        _buildCreateStar(),
+      ],
+    );
+  }
+
+  Widget _buildFlareActor(sprite, animation) {
+    return FlareActor(
+      sprite,
+      alignment: Alignment.center,
+      fit: BoxFit.fitWidth,
+      animation: animation,
     );
   }
 
   Widget _buildStars() {
     return SafeArea(
       child: Wrap(
-        spacing: 8.0,
-        runSpacing: 8.0,
-        direction: Axis.horizontal,
-        children: _starsModels.map((model)=>Star(model)).toList(),
+          direction: Axis.horizontal,
+          children: _starsModels
+              .map((model) => GestureDetector(
+                    onTap: () {
+                      setState(() => _starSelected = model);
+                      goToPage(0);
+                    },
+                    child: _buildStarIcon(model.starIcon, 60.0),
+                  ))
+              .toList()),
+    );
+  }
+
+  Widget _buildStarIcon(String sprite, double size) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: _buildFlareActor(sprite, "shine"),
+    );
+  }
+
+  Widget _buildSelected() {
+    return _starSelected != null
+        ? _buildMyCard(
+            <Widget>[
+              ListTile(
+                title: Text(_starSelected.title),
+                subtitle: Text(_starSelected.description),
+              ),
+              Container(
+                margin: EdgeInsets.all(16.0),
+                child: _starSelected.image,
+              ),
+            ],
+          )
+        : _buildStarIcon("assets/greenStar.flr", 64.0);
+  }
+
+  Widget _buildCreateStar() {
+    return _buildMyCard(
+      <Widget>[
+        TextField(
+          controller: tfTitle,
+          decoration: InputDecoration(labelText: "Title"),
+        ),
+        TextField(
+          controller: tfDesc,
+          decoration: InputDecoration(labelText: "Description"),
+        ),
+        Container(
+          height: 200.0,
+          child: GestureDetector(
+            onTap: () async {
+              imageNew = Image.file(
+                  await ImagePicker.pickImage(source: ImageSource.camera));
+              setState(() {});
+            },
+            child: imageNew,
+          ),
+        ),
+        RaisedButton(
+          child: Text("Add star"),
+          onPressed: () {
+            Star newStar = Star(
+                title: tfTitle.text,
+                description: tfDesc.text,
+                starIcon: "assets/purpleStar.flr",
+                image: imageNew);
+            setState(() {
+              imageNew = Image.asset("assets/noImage.png");
+              _starsModels.add(newStar);
+              goToPage(1);
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildMyCard(List<Widget> children) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: children,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/star.dart
+++ lib/star.dart
@@ -1,41 +1,10 @@
-import 'package:flare_flutter/flare_actor.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show Image;
 
-class StarModel{
+class Star {
   String title;
   String description;
   String starIcon;
-  DateTime date;
   Image image;
 
-  StarModel({this.title, this.description, this.starIcon, this.date,
-      this.image});
-}
-
-class Star extends StatefulWidget {
-  StarModel model;
-
-  Star(this.model);
-
-  @override
-  _StarState createState() => new _StarState();
-}
-
-class _StarState extends State<Star> {
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizedBox(
-      width: 50,
-      height: 50,
-      child: FlareActor(
-        widget.model.starIcon,
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: "shine",
-      ),
-    );
-  }
-
-
+  Star({this.title, this.description, this.starIcon, this.image});
 }
